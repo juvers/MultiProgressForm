6c817083df6e9acd6584e58e201b11f564d9cb73
Jude Kuti <judekuti@gmail.com>
2020-08-31 23:33:21 -0500

    Use app..js as main app entry, routes base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>Intial page</div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path='/' component={step1} />
+        <Route path='/step2' component={step2} />
+        <Route exact path='/step3' component={step3} />
+        <Route exact path='/result' component={result} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
