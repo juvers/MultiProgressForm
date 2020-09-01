08668b9243a9a35836a12ee4e7a2c05ff789ee68
Jude Kuti <judekuti@gmail.com>
2020-09-01 02:25:38 -0500

    Use useForm to get register functionality to be reffed and useHistory to push pages

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
 
 const Step1 = () => {
+    const { register, handleSubmit, errors } = useForm();
+
+    const history = useHistory();
+
+    const onSubmit = (data) => {
+        history.push('/step2');
+
+    }
     return (
-        <div>Step1 Page</div>
+        <>
+            <div>Step 2</div>
+            <form>
+                <input ref={register} name="firstName" type="text" placeholder="First Name" />
+                <input ref={register} name="lastName" type="text" placeholder="Last Name" />
+                <button type="submit">Next</button>
+            </form>
+        </>
     )
 }
 
