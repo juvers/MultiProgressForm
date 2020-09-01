3339f7595e1572533d5374dfef43253e0cd3d2bb
Jude Kuti <judekuti@gmail.com>
2020-09-01 00:18:45 -0500

    Create header component and place above all Routes

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Result from './components/Result';
 import Step1 from './components/Step1';
 import Step2 from './components/Step2';
 import Step3 from './components/Step3';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Step1} />
-        <Route path='/step2' component={Step2} />
-        <Route exact path='/step3' component={Step3} />
-        <Route exact path='/result' component={Result} />
-      </Switch>
-    </Router>
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Step1} />
+          <Route path='/step2' component={Step2} />
+          <Route exact path='/step3' component={Step3} />
+          <Route exact path='/result' component={Result} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
